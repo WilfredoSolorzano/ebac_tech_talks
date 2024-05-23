afd65385107e5adf079d1fb87e4199d7dacb240e
WilfredoSolorzano <wsolorzano00@gmail.com>
2024-05-23 20:20:49 -0300

    finalizando a contagem regresiva

---
+++ src/scripts/main.js
@@ -0,0 +1,32 @@
+AOS.init();
+
+const dataDoEvento=new Date("Dec 12, 2022 19:00:00");
+const timeStamDoEvento=dataDoEvento.getTime();
+
+const contaAsHoras=setInterval(function(){
+    const agora=new Date();
+    const timeStamDoAtual=agora.getTime();
+
+    const distaciaAteOEvento=timeStamDoEvento-timeStamDoAtual;
+
+    const diaEmMs=1000*60*60*24;
+    const horaEmMs=1000*60*60;
+    const minutoEmMs=1000*60;
+
+    const diasAteOEvento= Math.floor(distaciaAteOEvento / diaEmMs);
+    const horasAteOEvento=Math.floor((distaciaAteOEvento% diaEmMs) / horaEmMs)
+    const minutosAteOEvento=Math.floor((distaciaAteOEvento%horaEmMs)/minutoEmMs)
+    const segundosAteOEvento=Math.floor((distaciaAteOEvento% minutoEmMs)/1000);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML=`${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}`;
+
+    if(distaciaAteOEvento<0){
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML="evento Finalizado"
+    }
+},1000);
